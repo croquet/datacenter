8ca851aa3ef4fee9d64475b7ee22c5e51afd97d4
Croqueteer <davidasmith@gmail.com>
2022-12-11 21:55:53 -0800

    Added drawers, loading doors (not yet placed), new Data Center

--- behaviors/datacenter/cabinet.js
+++ behaviors/datacenter/cabinet.js
@@ -1,6 +1,16 @@
 class CabinetActor {
     setup(){
+        this.dynamicCards = [];
+        this.listen("createCard", this.doCreateCard);
+    }
 
+    doCreateCard(cardData){
+        if(this.dynamicCards.indexOf(cardData.name) === -1){
+            this.dynamicCards.push(cardData.name); // add it here
+            //cardData.parent = this; // set the parent (?)
+            console.log(cardData);
+            this.createCard(cardData);
+        }
     }
 }
 
@@ -13,11 +23,53 @@ class CabinetPawn {
     modelLoaded(){
         this.object = this.shape.children[0];
         console.log("Cabinet", this.object)
+        this.object.traverse((obj) => {
+            if(typeof obj.name === 'string'){
+                if(obj.name.search("Red")>=0){
+                } else if(obj.name.search("Green")>=0){
+                    if(obj.name.search("Blue")>=0){
+                        this.addLights(obj, [0x00ff00, 0x0000ff, 0x00ff00, 0x00ff00, 0x0000ff, 0x00ff00, 0x0000ff, 0x00ff00]);
+                    }else{
+                        this.addLights(obj, [0x00ff00]);
+                    }
+                } else if (obj.name.search("Blue")>=0){
+                    this.addLights(obj, 0x0000ff);
 
+                } else if(obj.name.search("rackhinge")>=0){
+                    this.addDoor(obj);
+
+                }
+            }
+        });
        // this.listen("updateWorld", this.update);
     }
-}
 
+    addLights( obj, colors ){
+        //console.log(obj.name, colors);
+    }
+
+    addDoor(obj){
+        console.log("addDoor", obj.name)
+        if(this.actor.dynamicCards.indexOf(obj.name) === -1){
+            this.createCard({
+                name: obj.name,
+                layers: ["walk", "pointer"],
+                singleSided: false,
+                shadow: true,
+                translation:[0, -1.7, 0],
+                type: "3d",
+                fileName: "/DC_cabinet.glb",
+                dataLocation: "./assets/3D/DC_cabinet.glb",
+                behaviorModules: ["Hinge"],
+            });
+        }
+    }
+
+    createCard(cardDef){
+        this.say("createCard", cardDef);
+    }
+
+}
 
 export default {
     modules: [
