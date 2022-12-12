3c7ef016dfeab39d82a9d33dd461e4fcb1b997fb
Vanessa Freudenberg <vanessa@codefrau.net>
2022-12-12 00:31:10 -0800

    Rotate relative to initial door pos

--- behaviors/datacenter/hinge.js
+++ behaviors/datacenter/hinge.js
@@ -1,5 +1,6 @@
 class HingeActor {
     setup(){
+        if (!("doorYaw" in this)) this.doorYaw = Microverse.q_yaw(this.rotation);
         this.doorState = 0;
         this.doorDelta = 0;
         this.addEventListener("pointerDown", "toggle");
@@ -21,7 +22,7 @@ class HingeActor {
             if (this.doorState < 0) { this.doorState = 0; this.doorDelta = 0; }
             if (this.doorState > 1) { this.doorState = 1; this.doorDelta = 0; }
 
-            const rotation = Math.PI - this.doorState * Math.PI / 2;
+            const rotation = this.doorYaw - this.doorState * Math.PI / 2;
             const q = Microverse.q_euler(0, rotation, 0);
             this.rotateTo(q);
 
